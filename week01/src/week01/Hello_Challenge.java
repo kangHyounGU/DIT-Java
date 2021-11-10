056048e2062664c5c9f4e44c92d5e553ce3bc8e1
kangHyeonGu <rkdgusrn310@gmail.com>
2021-11-11 02:50:02 +0900

    week01 LAB

---
+++ week01/src/week01/Hello_Challenge.java
@@ -0,0 +1,14 @@
+package week01;
+
+public class Hello_Challenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//자바의 특징 중에서 3개를 콘솔 창에 출력하도록 프로그램을 변경하여 보자.
+		System.out.println("객체 지향 프로그래밍");
+		System.out.println("자동 메모리 관리");
+		System.out.println("운영체제에 독립적");
+
+	}
+
+}
